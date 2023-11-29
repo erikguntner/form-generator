98c7e56db4e1f36fcfd2d120f56b4423214e7a87
Erik <eguntner2291@gmail.com>
2023-11-29 09:31:10 -0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+
+//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
