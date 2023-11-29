3f7c20d9072fe43451c046fb899bdac0c1129c93
Erik <eguntner2291@gmail.com>
2023-11-29 11:48:17 -0800

    Create first components

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -1,24 +1,45 @@
-import {Button} from '@mui/material';
-import {useState} from 'react';
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Stack,
+  Typography,
+} from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const createApplication = () => {
+    console.log('createApplication');
+  };
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div>
-        <Button
-          variant="contained"
-          onClick={() => setCount(count => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'grey.50',
+        py: 3,
+      }}
+    >
+      <Container>
+        <Stack spacing={3}>
+          <Typography variant="h1" sx={{fontSize: 28}}>
+            My Applications
+          </Typography>
+          <Stack direction="row">
+            <Button variant="contained" onClick={createApplication}>
+              Create Application
+            </Button>
+          </Stack>
+          <Divider />
+        </Stack>
+        <Stack sx={{pt: 3}}>
+          <Typography variant="h2" sx={{fontSize: 24}}>
+            No Applications
+          </Typography>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
 
--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import {red} from '@mui/material/colors';
 import {createTheme} from '@mui/material/styles';
 
@@ -5,13 +10,35 @@ import {createTheme} from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#0072f5',
     },
     secondary: {
       main: '#19857b',
     },
     error: {
       main: red.A400,
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+        disableRipple: true,
+      },
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+
+        outlinedSecondary: {
+          color: 'black',
+          borderColor: 'black',
+          '&:hover': {
+            backgroundColor: 'black',
+            color: '#fff',
+          },
+        },
+      },
     },
   },
 });
