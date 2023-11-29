054f3862a50be3efa774a7e493250e73d9908efa
Erik <eguntner2291@gmail.com>
2023-11-29 09:40:06 -0800

    Setup testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-
-//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
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/utils/test/setupTests.ts",
+  },
+});
