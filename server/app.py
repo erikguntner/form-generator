7fb288bc369bf22c86423a63e0d498da6741950e
Erik <eguntner2291@gmail.com>
2023-12-07 16:25:01 -0800

    Remove old db

--- server/app.py
+++ server/app.py
@@ -3,8 +3,5 @@ from init import create_app
 
 app = create_app()
 
-# print sql alchemy uri
-print(app.app.config["SQLALCHEMY_DATABASE_URI"])
-
 if __name__ == "__main__":
     app.run(f"{Path(__file__).stem}:app", port=8080, reload=True)
