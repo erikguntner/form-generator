c34119dde6412ff38db61a80494b6216a21b3980
Erik <eguntner2291@gmail.com>
2023-12-08 23:32:08 -0800

    Add React Router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,6 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 import App from './features/App.tsx';
 import {setupStore} from './redux/store.ts';
@@ -12,12 +13,19 @@ import theme from './utils/theme.tsx';
 
 const store = setupStore();
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
