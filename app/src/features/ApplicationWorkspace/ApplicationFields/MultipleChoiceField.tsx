6dc7c283cf3f1cbc2eb1f8d36344e52f1503c6c7
Erik <eguntner2291@gmail.com>
2024-01-11 23:15:21 -0800

    Add numbers and delete button

--- app/src/features/ApplicationWorkspace/ApplicationFields/MultipleChoiceField.tsx
+++ app/src/features/ApplicationWorkspace/ApplicationFields/MultipleChoiceField.tsx
@@ -1,13 +1,86 @@
-import {List, ListItem} from '@mui/material';
+import {faker} from '@faker-js/faker';
+import ClearIcon from '@mui/icons-material/Clear';
+import {
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  Stack,
+  TextField,
+} from '@mui/material';
+import {styled} from '@mui/material/styles';
+import {useState} from 'react';
+
+import {Choice} from '../workspaceSlice';
 
 interface MultipleChoiceFieldProps {}
 
 export const MultipleChoiceField = ({}: MultipleChoiceFieldProps) => {
+  const [choices, setChoices] = useState<Choice[]>([
+    {
+      id: faker.string.uuid(),
+      label:
+        'choice 1fasdfak;lsdfha;lskjdfhal;ksjdflkasjdf lkajsdfl;aksdj fasl;kdfj as;dlfkj asdlfj als;dfj asd;ljf asldfj asdfjaslkdfja ;sfaslkdj fal;skdfj ',
+    },
+    {id: faker.string.uuid(), label: 'choice 2'},
+    {id: faker.string.uuid(), label: 'choice 3'},
+  ]);
+
   return (
-    <List>
-      <ListItem>1</ListItem>
-      <ListItem>2</ListItem>
-      <ListItem>3</ListItem>
-    </List>
+    <Stack gap={2} alignItems="start">
+      <List disablePadding>
+        {choices.map(({id, label}, index) => {
+          return (
+            <ListItem disableGutters key={id}>
+              <StyledListNumber>{index + 1}</StyledListNumber>
+              <StyledTextField
+                fullWidth
+                id="outlined"
+                variant="outlined"
+                placeholder="Type your choice here"
+                value={label}
+                multiline
+              />
+              <StyledIconButton size="small" aria-label="delete">
+                <ClearIcon fontSize="inherit" />
+              </StyledIconButton>
+            </ListItem>
+          );
+        })}
+      </List>
+      <Button variant="outlined">Add Choice</Button>
+    </Stack>
   );
 };
+
+const StyledIconButton = styled(IconButton)(({theme}) => ({
+  backgroundColor: theme.palette.grey[700],
+  color: 'white',
+  fontSize: 14,
+  '&:hover': {
+    backgroundColor: theme.palette.grey[600],
+  },
+  position: 'absolute',
+  right: 0,
+  transform: 'translateX(50%)',
+}));
+
+const StyledTextField = styled(TextField)(({theme}) => ({
+  '& .MuiOutlinedInput-root': {
+    paddingLeft: theme.spacing(6),
+  },
+}));
+
+const StyledListNumber = styled(Box)(({theme}) => ({
+  position: 'absolute',
+  left: `${theme.spacing(1.5)}`,
+  top: `${theme.spacing(2.5)}`,
+  border: '1px solid',
+  borderColor: theme.palette.grey[300],
+  padding: `${theme.spacing(1)} ${theme.spacing(1)}`,
+  marginRight: theme.spacing(1),
+  lineHeight: 1,
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: theme.palette.grey[100],
+}));
