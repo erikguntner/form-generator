2e9710f963a95f848f1ba91444e463d6e84cda35
Erik <eguntner2291@gmail.com>
2024-03-13 18:31:21 -0700

    Add field selection button to center window and render selected field group

--- app/src/features/ApplicationWorkspace/CenterWindow.tsx
+++ app/src/features/ApplicationWorkspace/CenterWindow.tsx
@@ -1,86 +1,134 @@
+import {faker} from '@faker-js/faker';
 import {Container, Stack, styled, Typography} from '@mui/material';
 import ContentEdidable from 'react-contenteditable';
 
 import {useAppDispatch, useAppSelector} from '../../redux/hooks';
-// import {RenderFields} from './RenderApplicationFields';
-import {editFieldGroup, Fields} from './workspaceSlice';
+import {AddFieldButton} from './AddFieldButton';
+import {RenderFields} from './RenderApplicationFields';
+import {
+  addField,
+  editField,
+  editFieldGroup,
+  Fields,
+  FieldTypes,
+} from './workspaceSlice';
 
 export const CenterWindow = () => {
   const dispatch = useAppDispatch();
-  const selectedFieldGroup = useAppSelector(({workspace}) =>
+  const fieldGroup = useAppSelector(({workspace}) =>
     workspace.fieldGroups.find(
       fieldGroup => fieldGroup.id === workspace.selectedId
     )
   );
 
+  if (fieldGroup === undefined) {
+    return (
+      <Stack
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
+          height: '100%',
+        }}
+      >
+        <Typography variant="h5" sx={{color: 'text.secondary'}}>
+          Create a new field group
+        </Typography>
+      </Stack>
+    );
+  }
+
   const handleOnChange = (options: Partial<Fields>) => {
     dispatch(
       editFieldGroup({
-        id: selectedFieldGroup?.id,
+        id: fieldGroup.id,
         ...options,
       })
     );
   };
 
+  const handleEditField = ({id, title}: {id: string; title: string}) => {
+    console.log('editing field');
+    dispatch(editField({groupId: fieldGroup.id, id, field: {title}}));
+  };
+
+  const addFields = (fieldType: FieldTypes) => {
+    const field: Fields = {
+      id: faker.string.uuid(),
+      type: fieldType,
+      title: '',
+      properties: {
+        description: '',
+      },
+      validations: {},
+    };
+
+    if (fieldType === 'dropdown') {
+      field.properties.choices = [];
+    }
+
+    if (fieldType === 'multiple_choice') {
+      field.properties.choices = [
+        {
+          id: faker.string.uuid(),
+          label: 'choice 1',
+        },
+        {
+          id: faker.string.uuid(),
+          label: 'choice 2',
+        },
+        {
+          id: faker.string.uuid(),
+          label: 'choice 3',
+        },
+      ];
+    }
+
+    dispatch(addField({id: fieldGroup.id, field}));
+  };
+
+  console.log({fieldGroup});
+
   return (
     <Stack sx={{flex: 1, py: 10, px: 2}}>
       <StyledWindow>
-        {selectedFieldGroup === undefined ? (
+        <Container sx={{height: '100%'}} maxWidth="sm">
           <Stack
-            sx={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              flex: 1,
-              height: '100%',
-            }}
+            sx={{height: '100%', paddingTop: 4, overflowY: 'auto'}}
+            justifyContent={'start'}
+            gap={3}
           >
-            <Typography variant="h5" sx={{color: 'text.secondary'}}>
-              Create a new field group
-            </Typography>
-          </Stack>
-        ) : (
-          <Container sx={{height: '100%'}} maxWidth="sm">
-            <Stack
-              sx={{height: '100%', paddingTop: 4}}
-              justifyContent={'start'}
-              gap={3}
-            >
-              <Stack>
-                <Typography aria-labelledby="editable-title" variant="h5">
-                  <StyledContentEditable
-                    onChange={event =>
-                      handleOnChange({title: event.currentTarget.textContent})
-                    }
-                    data-placeholder="Write your title here"
-                    html={selectedFieldGroup.title}
-                  />
-                </Typography>
-                {/* 
-                  <StyledContentEditable
-                    onChange={event =>
-                      handleOnChange({title: event.currentTarget.textContent})
-                    }
-                    data-placeholder="Write your question here"
-                    html={selectedFieldGroup.title}
-                  />
+            <Stack sx={{gap: 3}}>
+              <Typography aria-labelledby="editable-title" variant="h5">
                 <StyledContentEditable
-                  aria-labelledby="editable-description"
-                  sx={{color: 'text.secondary'}}
                   onChange={event =>
-                    handleOnChange({
-                      properties: {
-                        description: event.currentTarget.textContent,
-                      },
-                    })
+                    handleOnChange({title: event.currentTarget.textContent})
                   }
-                  data-placeholder="Write a desecription (optional)"
-                  html={selectedField.properties.description || ''}
-                /> */}
-              </Stack>
-              {/* <RenderFields field={selectedField} /> */}
+                  data-placeholder="Write your title here"
+                  html={fieldGroup.title}
+                />
+              </Typography>
+              {fieldGroup.fields.map(field => {
+                return (
+                  <Stack key={field.id} sx={{gap: 2}}>
+                    <StyledContentEditable
+                      onChange={event =>
+                        handleEditField({
+                          id: field.id,
+                          title: event.currentTarget.textContent,
+                        })
+                      }
+                      data-placeholder="Write your question here"
+                      html={field.title}
+                    />
+                    <RenderFields field={field} />
+                  </Stack>
+                );
+              })}
+              <AddFieldButton addField={addFields} />
             </Stack>
-          </Container>
-        )}
+          </Stack>
+        </Container>
       </StyledWindow>
     </Stack>
   );
