42f9f2531209ace3a6327ad3e97255a0cb8b01d6
Erik <eguntner2291@gmail.com>
2024-03-13 18:30:23 -0700

    Remove add field button from left panel

--- app/src/features/ApplicationWorkspace/LeftPanel.tsx
+++ app/src/features/ApplicationWorkspace/LeftPanel.tsx
@@ -5,18 +5,14 @@ import {MouseEvent as ReactMouseEvent} from 'react';
 
 import {useAppDispatch, useAppSelector} from '../../redux/hooks';
 import {Tooltip} from '../Common';
-import {AddFieldButton} from './AddFieldButton';
 import {FieldGroupList} from './FieldGroupList';
 import {PanelContainer} from './PanelContainer';
-import {FieldTypes} from './workspaceSlice';
-import {addField, addFieldGroup, Fields, setSelectedId} from './workspaceSlice';
+import {addFieldGroup, setSelectedId} from './workspaceSlice';
 
 export const LeftPanel = () => {
   const selectedId = useAppSelector(state => state.workspace.selectedId);
   const fieldGroups = useAppSelector(state => state.workspace.fieldGroups);
   const dispatch = useAppDispatch();
-
-  console.log({fieldGroups});
 
   const selectFieldById = (
     event: ReactMouseEvent<HTMLDivElement, MouseEvent>,
@@ -29,42 +25,6 @@ export const LeftPanel = () => {
     const id = faker.string.uuid();
     dispatch(addFieldGroup({id}));
     dispatch(setSelectedId(id));
-  };
-
-  const addFields = (fieldType: FieldTypes) => {
-    const newField: Fields = {
-      id: faker.string.uuid(),
-      type: fieldType,
-      title: '',
-      properties: {
-        description: '',
-      },
-      validations: {},
-    };
-
-    if (fieldType === 'dropdown') {
-      newField.properties.choices = [];
-    }
-
-    if (fieldType === 'multiple_choice') {
-      newField.properties.choices = [
-        {
-          id: faker.string.uuid(),
-          label: 'choice 1',
-        },
-        {
-          id: faker.string.uuid(),
-          label: 'choice 2',
-        },
-        {
-          id: faker.string.uuid(),
-          label: 'choice 3',
-        },
-      ];
-    }
-
-    dispatch(addField(newField));
-    dispatch(setSelectedId(newField.id));
   };
 
   return (
@@ -98,7 +58,6 @@ export const LeftPanel = () => {
             <Add sx={{height: '18px'}} />
           </Button>
         </Tooltip>
-        <AddFieldButton addField={addFields} />
       </Stack>
       <FieldGroupList
         fieldGroups={fieldGroups}
